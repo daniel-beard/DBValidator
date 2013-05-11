2420bcea0228a4537f8f235983a466f8a9790ab1
Daniel Beard <daniel.beard0@gmail.com>
2013-05-11 16:28:39 +0800

    Added unit test target
    Fixes #3

---
+++ DBValidatorTests/DBValidatorTests.h
@@ -0,0 +1,13 @@
+//
+//  DBValidatorTests.h
+//  DBValidatorTests
+//
+//  Created by Daniel Beard on 11/05/13.
+//  Copyright (c) 2013 Daniel Beard. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DBValidatorTests : SenTestCase
+
+@end
---
+++ DBValidatorTests/DBValidatorTests.m
@@ -0,0 +1,32 @@
+//
+//  DBValidatorTests.m
+//  DBValidatorTests
+//
+//  Created by Daniel Beard on 11/05/13.
+//  Copyright (c) 2013 Daniel Beard. All rights reserved.
+//
+
+#import "DBValidatorTests.h"
+
+@implementation DBValidatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DBValidatorTests");
+}
+
+@end
